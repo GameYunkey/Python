0fc1d4085b613abf104e3daed623e6b966cce1de
GameYunkey <113859186+GameYunkey@users.noreply.github.com>
2024-09-29 22:57:40 +0200

    Update skilltree.py
    Rechnung für Level 1 - 50

--- skilltree.py
+++ skilltree.py
@@ -1,8 +1,20 @@
 #------------------------------------------------#
+#LEVEL 1 = 10 - LEVEL 25 = 100
+## Werte einstellung für den Levelaufstieg
+# Startwerte
+L1 = 10  # Anzahl Liegestütze am ersten Tag
+r = 0.10  # Wachstumsrate von 20%w
+tage = 25  # Anzahl Tage
+liegestuetzen = L1 * (1 + r) ** (tage - 1)
+print(liegestuetzen)
+#------------------------------------------------#
+
+#------------------------------------------------#
+#LEVEL 25 = 100 - LEVEL 50 = 325
 ## Werte einstellung für den Levelaufstieg
 # Startwerte
 L1 = 100  # Anzahl Liegestütze am ersten Tag
-r = 0.10  # Wachstumsrate von 20%w
+r = 0.05  # Wachstumsrate von 20%w
 tage = 25  # Anzahl Tage
 
 liegestuetzen = L1 * (1 + r) ** (tage - 1)
